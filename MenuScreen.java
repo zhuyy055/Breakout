f5ac93246e59509490aceb4b9729ae38c358735a
zhuyy055 <31638320+zhuyy055@users.noreply.github.com>
2019-04-14 19:39:23 +0930

    v1.4

--- MenuScreen.java
+++ MenuScreen.java
@@ -6,9 +6,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 
 public class MenuScreen implements Screen {
@@ -37,7 +39,12 @@ public class MenuScreen implements Screen {
         playgamebutton.setHeight(Gdx.graphics.getHeight()*0.2f);
         playgamebutton.setPosition(0 ,
                 Gdx.graphics.getHeight() - playgamebutton.getHeight());
-
+        playgamebutton.addListener( new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen( new GameScreen(game) );
+            }
+        } );
         stage.addActor(playgamebutton);
         Gdx.input.setInputProcessor(stage);
 
@@ -47,7 +54,12 @@ public class MenuScreen implements Screen {
         exitgamebutton.setHeight(Gdx.graphics.getHeight()*0.2f);
         exitgamebutton.setPosition(Gdx.graphics.getWidth() - exitgamebutton.getWidth(),
                 0);
-
+        exitgamebutton.addListener( new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.exit(0);
+            }
+        } );
         stage.addActor(exitgamebutton);
         Gdx.input.setInputProcessor(stage);
 
